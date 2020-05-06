65e5639663ebaaddb0575855659e5688e895c5ba
anushiya-thevapalan <anushiya.16@cse.mrt.ac.lk>
2020-05-06 17:17:43 +0530

    adding Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>John Doe</h4>
+        <ul>
+          <li>Email: john@gmail.com</li>
+          <li>Phone: 111-111-1111</li>>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Contact;
