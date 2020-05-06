b78358b1e7c7c61d8b2a16188f25e3b57d5c464c
anushiya-thevapalan <anushiya.16@cse.mrt.ac.lk>
2020-05-06 17:06:26 +0530

    cleaning up basic code snippets

--- src/App.js
+++ src/App.js
@@ -2,24 +2,9 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const name = 'Shaaya'
-  const showHello = true
-  const showMath = true
-
-  let math
-  if (showMath){
-    math = <h4>5 + 1 = {5+1} </h4>
-  } else {
-     math = null
-  }
   return (
     <div className="App">
       <h1>Welcome to React</h1>
-      <h3>Hello {name.toUpperCase()} </h3>
-      {/* if showHello is True, greet, else do nothing */}
-      {showHello ? <h4>Welcome back {name.toUpperCase()} </h4> : null}
-      <h4>1 + 1 = {1+1} </h4>
-      {math}
     </div>
   );
 
