f4e612ecfb2b46f7763aa36355acbe3b1242311b
anushiya-thevapalan <anushiya.16@cse.mrt.ac.lk>
2020-05-06 17:30:43 +0530

    adding functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Contact from './components/Contact'
+import Header from './components/Header'
+
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React</h1>
+      <Header/>
       <Contact/>
     </div>
   );
