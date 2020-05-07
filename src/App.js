80f4c695d96948662277cb01a557bcaaa25f7eee
anushiya-thevapalan <anushiya.16@cse.mrt.ac.lk>
2020-05-07 13:21:33 +0530

    adding bootstrap styling to components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import './App.css';
 import Contact from './components/Contact'
 import Header from './components/Header'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header branding="Contact Manager" />
+      <div className="container">
       <Contact name="John Doe" email="john@gmail.com" phone="111-111-1111" />
+      <Contact name="Mary Doe" email="mary@gmail.com" phone="222-222-2222" />
+      </div>
+      
     </div>
   );
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,25 @@ import PropTypes from 'prop-types'
 function Header(props) {
   const {branding} = props;
   return (
-    <div>
-      <h1> {props.branding} </h1>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    // <div>
+    //   <h1> {props.branding} </h1>
+    // </div>
   )
 }
 
